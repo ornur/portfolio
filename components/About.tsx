f36205c2e5dc6b77127f232baadf31a00a860a48
ornur <nurdauletaidaruli@gmail.com>
2025-07-16 03:59:22 +0500

    Refactor About component: remove intersection observer logic and simplify structure for improved readability

--- components/About.tsx
+++ components/About.tsx
@@ -1,35 +1,28 @@
 "use client";
 
-import { useInView } from "react-intersection-observer";
 import { BlurIn } from "./ui/blur-in";
 
 const About = () => {
-  const { ref, inView } = useInView({
-    triggerOnce: false,
-    threshold: 0.1,
-  });
 
   return (
-    <div ref={ref} className="mt-96 flex justify-center">
-      {inView && (
-        <div className="flex flex-col items-center gap-4">
-          <BlurIn
-            word="Frontend Developer"
-            className="font-semibold w-[90%] md:w-[60%]"
-            useAnimatedBackground
-            useMask
-            duration={2}
-            delay={0}
-          />
-          <BlurIn
-            description
-            word="I am an experienced Frontend developer specializing in creating intuitive, responsive, and high-performance user interfaces. My main goal is to turn complex concepts into user—friendly and functional applications, while paying special attention to usability, performance, and code quality.
+    <div className="mt-96 flex justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <BlurIn
+          word="Frontend Developer"
+          className="font-semibold"
+          useAnimatedBackground
+          useMask
+          duration={2}
+          delay={0}
+        />
+        <BlurIn
+          description
+          word="I am an experienced Frontend developer specializing in creating intuitive, responsive, and high-performance user interfaces. My main goal is to turn complex concepts into user—friendly and functional applications, while paying special attention to usability, performance, and code quality.
 I have experience working as a Frontend developer at ZIZ INC. I am currently actively looking for opportunities for further professional growth and participation in the development of meaningful and thoughtful digital products.
 Ready to work remotely. I am in Astana, Kazakhstan."
-            className="w-[90%] font-normal md:w-[60%] tracking-tight leading-8"
-          />
-        </div>
-      )}
+          className="font-normal md:w-[120%] tracking-tight leading-6 md:leading-8"
+        />
+      </div>
     </div>
   );
 };
