2dd3cf22a73a128d3a11830f4a74627993bb8148
ornur <nurdauletaidaruli@gmail.com>
2025-11-22 21:49:03 +0500

    feat: Add initial About, Experience, Projects, and Footer components to build out the portfolio layout.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="w-full h-[25vh] flex items-end pb-20 justify-center text-center text-sm text-zinc-500" >
+    <footer className="w-full h-[20vh] md:h-[25vh] flex items-end pb-20 justify-center text-center text-sm text-zinc-500" >
       © {currentYear} Nurdaulet Orynbassarov
     </footer>
   );
