e3f587b47548edcbf7251f5d0a907b34045796ca
ornur <nurdauletaidaruli@gmail.com>
2025-11-22 21:02:50 +0500

    feat: add core portfolio sections including About, Experience, Projects, and Footer components.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,8 @@
-import Link from "next/link";
-import { RainbowButton } from "./ui/rainbow-button";
-
-const Footer = () => {
+export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="w-full">
-      <div className="my-35 text-center">
-        <p className="text-zinc-500">Need a Web Developer?</p>
-        <h1 className="mb-5 text-4xl font-bold">Let&apos;s Work Together</h1>
-        <RainbowButton>
-          <Link href="mailto:nurdauletaidaruli@gmail.com">Hire Me</Link>
-        </RainbowButton>
-      </div>
-      <div className="mb-20 text-center text-sm text-zinc-500">
-        <p>© {currentYear} Nurdaulet Orynbassarov</p>
-      </div>
+    <footer className="w-full h-[25vh] flex items-end pb-20 justify-center text-center text-sm text-zinc-500" >
+      © {currentYear} Nurdaulet Orynbassarov
     </footer>
   );
 };
-
-export default Footer;
